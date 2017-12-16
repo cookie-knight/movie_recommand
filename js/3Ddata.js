566c2087dc65f5eeb03c8c6bae27977ac7f5a4ee
cookie-brown <1394956763@qq.com>
2017-12-16 17:21:35 +0800

    除了个人中心和院线比较。。

--- js/3Ddata.js
+++ js/3Ddata.js
@@ -3,54 +3,54 @@ var data = {
 	letv:[
 		{
 			"ul":"html/single_film.html",
-			"title":"Picture information1",
+			"title":"名侦探柯南---唐红的恋歌",
 			"date":"2017.02.01日 11:30",
 			"id":11,
 			"name":"张三",
-			"img":"img/01.png"
+			"img":"img/主页03.jpg"
 		},		
 		{
 			"ul":"html/single_film.html",
-			"title":"Picture information2",
+			"title":"名侦探柯南---沉默的十五分钟",
 			"date":"2017.02.01日 11:30",
 			"id":12,
 			"name":"张三",
-			"img":"img/02.png"
+			"img":"img/主页06.jpg"
 		},			
 		{
 			"ul":"html/single_film.html",
-			"title":"Picture information3",
+			"title":"名侦探柯南---业火的向日葵",
 			"date":"2017.02.03日 11:30",
 			"id":13,
-			"img":" img/03.png"
+			"img":" img/主页07.jpg"
 		},
 		{
 			"ul":"html/single_film.html",
-			"title":"Picture information4",
+			"title":"名侦探可那---纯黑的噩梦",
 			"date":"2017.02.04日 11:30",
 			"id":14,
-			"img":" img/04.png"
+			"img":" img/主页05.jpg"
 		},
 		{
 			"ul":"html/single_film.html",
-			"title":"Picture information5",
+			"title":"柯哀王道",
 			"date":"2017.02.05日 11:30",
 			"id":15,
-			"img":" img/05.png"
+			"img":" img/主页02.jpg"
 		},
 		{
 			"ul":"html/single_film.html",
-			"title":"Picture information6",
+			"title":"名侦探柯南---战栗的乐谱",
 			"date":"2017.05.06日 11:30",
 			"id":16,
-			"img":" img/06.png"
+			"img":" img/主页08.jpg"
 		},
 		{
 			"ul":"html/single_film.html",
-			"title":"Picture information7",
+			"title":"柯哀只要有你在",
 			"date":"2017.04.07日 11:30",
 			"id":17,
-			"img":" img/07.png"
+			"img":" img/主页01.jpg"
 			
 		},
 	
